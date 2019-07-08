b5e2036f6aa4d5a0d541d370efe72b679b42f6ff
besandr <grandeus@gmail.com>
2019-07-09 00:43:33 +0300

    added UnknownCommandTest class

---
+++ src/test/java/com/library/web/controller/commands/UnknownCommandTest.java
@@ -0,0 +1,34 @@
+package com.library.web.controller.commands;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UnknownCommandTest {
+
+    @Test
+    public void processShouldCallForwardWithStringError() throws Exception{
+
+        FrontCommand spyCommand = spy(new UnknownCommand());
+        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+
+        ArgumentCaptor valueCapture = ArgumentCaptor.forClass(String.class);
+
+        doNothing().when(spyCommand).forward((String)valueCapture.capture());
+        doCallRealMethod().when(spyCommand).process();
+        when(mockRequest.getParameter(RequestParamsMap.COMMAND)).thenReturn("unknown");
+
+        spyCommand.init(null, mockRequest, null);
+        spyCommand.process();
+
+        assertEquals("error", valueCapture.getValue());
+    }
+
+}
