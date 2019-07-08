a9fa0abb34ddf9143bdcea545c05432e0d3f80fb
besandr <grandeus@gmail.com>
2019-07-09 00:43:15 +0300

    added UnknownCommand class

---
+++ src/main/java/com/library/web/controller/commands/UnknownCommand.java
@@ -0,0 +1,26 @@
+package com.library.web.controller.commands;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * Represents an unknown command received from request
+ */
+public class UnknownCommand extends FrontCommand {
+
+    private static final Logger log = LogManager.getLogger(UnknownCommand.class);
+
+    /**
+     * Logs exception situation with unknown command and forwards
+     * to error page
+     */
+    @Override
+    public void process() throws ServletException, IOException {
+        log.error(String.format("Received unknown command \"%s\" from view.", request.getParameter(RequestParamsMap.COMMAND)));
+        forward("error");
+    }
+
+}
