bf502a4c99b17d57ad1691666fdde3888881c6d4
besandr <grandeus@gmail.com>
2019-07-09 13:43:01 +0300

    added StartApplicationListener class

---
+++ src/main/java/com/library/web/listeners/StartApplicationListener.java
@@ -0,0 +1,30 @@
+package com.library.web.listeners;
+
+import com.library.web.controller.commands.RequestParamsMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Configures application on startup
+ */
+public class StartApplicationListener implements ServletContextListener{
+
+    // Public constructor is required by servlet spec
+    public StartApplicationListener() {
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        ServletContext context = sce.getServletContext();
+
+        //Set application scope attribute with request parameters names
+        context.setAttribute("requestParamsMap", new RequestParamsMap());
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
