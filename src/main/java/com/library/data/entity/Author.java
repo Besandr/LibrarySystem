9d0a5b63d904749916a47c9489e3fa55dd14ee7a
besandr <grandeus@gmail.com>
2019-07-12 18:53:12 +0300

    added @Builder annotation

--- src/main/java/com/library/data/entity/Author.java
+++ src/main/java/com/library/data/entity/Author.java
@@ -1,11 +1,16 @@
 package com.library.data.entity;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
+@Builder
 public class Author {
 
     private long id;
 
-    private String name;
+    private String firstName;
+
+    private String lastName;
 }
