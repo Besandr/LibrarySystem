fd2b4d1a3ee587f7132b2fd85d5d3a0229e14b03
besandr <grandeus@gmail.com>
2019-07-17 09:31:33 +0300

    in AuthorDao and KeywordDao added finder methods by entity's parameters

--- src/main/java/com/library/model/data/dao/AuthorDao.java
+++ src/main/java/com/library/model/data/dao/AuthorDao.java
@@ -2,5 +2,16 @@ package com.library.model.data.dao;
 
 import com.library.model.data.entity.Author;
 
+import java.util.Optional;
+
 public interface AuthorDao extends Dao<Author> {
+
+    /**
+     * Tries to find an author with given first and last names
+     * @param firstName - the first name of the target author
+     * @param lastName - the last name of the target author
+     * @return - an Optional with the target author or an
+     *          empty Optional if there is no such author in DB
+     */
+    Optional<Author> getByName(String firstName, String lastName);
 }
--- src/main/java/com/library/model/data/dao/KeywordDao.java
+++ src/main/java/com/library/model/data/dao/KeywordDao.java
@@ -2,5 +2,15 @@ package com.library.model.data.dao;
 
 import com.library.model.data.entity.Keyword;
 
+import java.util.Optional;
+
 public interface KeywordDao extends Dao<Keyword> {
+
+    /**
+     * Tries to find a keyword in a DB by given word
+     * @param word - the word which must be in a keyword row
+     * @return - an Optional with the target keyword or an
+     *          empty Optional if there is no such keyword in DB
+     */
+    Optional<Keyword> getByWord(String word);
 }
