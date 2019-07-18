535c96a4511537c0357c08735cbf2293aae2698d
besandr <grandeus@gmail.com>
2019-07-18 23:35:06 +0300

    added comments to LocationDao

--- src/main/java/com/library/model/data/dao/LocationDao.java
+++ src/main/java/com/library/model/data/dao/LocationDao.java
@@ -5,12 +5,34 @@ import com.library.model.data.entity.Location;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Dao for Books entity
+ */
 public interface LocationDao extends Dao<Location> {
 
+    /**
+     * Saves book to target(free) location.
+     * @param locationId - id of free location
+     * @param bookId - id of book to be saved
+     */
     void saveBookToLocation(long locationId, long bookId);
 
+    /**
+     * Changes is_occupied state of given location
+     * @param locationId - id of location to be changed
+     * @param isOccupied - new status of location
+     *                   false - for taking a book from location to user
+     *                   true - for returning a book to location
+     */
     void updateIsOccupied(long locationId, boolean isOccupied);
 
+    /**
+     * Gets the quantity of given id books in the storage (present and
+     * borrowed)
+     * @param book_id - id of book which locations need to be counted
+     * @return - quantity of target books in the storage(present and
+     * borrowed)
+     */
     int getBookQuantity(long book_id);
 
     /**
