4c7218098a0d1ec4f0f2fba0c4071036d484d639
Besandr <grandeus@gmail.com>
2019-07-26 00:47:09 +0300

    created ActionServletConfigurator and its test class

---
+++ src/main/java/com/library/web/controller/ActionServletConfigurator.java
@@ -0,0 +1,113 @@
+package com.library.web.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.util.Optional;
+
+/**
+ * Configurator of {@code ActionServlet}.
+ *
+ * Main stages of configuring:
+ * - load servlet configuration from xml config file
+ * - set up {@code ActionFactory} and {@code FormFactory}
+ * - create facade class {@code ServletResources} and
+ * give it to {@code ActionServlet}
+ */
+class ActionServletConfigurator {
+
+    private static final Logger log = LogManager.getLogger(ActionServletConfigurator.class);
+
+    /**
+     * Creates {@code ServletResources} from xml config file.
+     * @return created {@code ServletResources}
+     */
+    ServletResources createServletResources(){
+        ActionServletConfig actionServletConfig = loadConfigFromFile();
+
+        setUpFactories(actionServletConfig,
+                ActionFactory.getInstance(),
+                FormFactory.getInstance());
+
+        return new ServletResources(
+                ActionFactory.getInstance(),
+                FormFactory.getInstance(),
+                actionServletConfig.getForwards());
+    }
+
+    /**
+     * Loads configuration data from xml config file and converts.
+     * it into {@code ActionServletConfig} object and initialize
+     * variable {@code actionServletConfig} by it.
+     * {@code RuntimeException} may be thrown if during loading and
+     * convertation errors occurred
+     * @return created config object
+     */
+    private ActionServletConfig loadConfigFromFile(){
+        InputStream is = ActionServletConfigurator.class.getClassLoader().getResourceAsStream("servlet-config.xml");
+        try {
+            JAXBContext jContext = JAXBContext.newInstance(ActionServletConfig.class);
+            Unmarshaller jUnmarshaller = jContext.createUnmarshaller();
+            return (ActionServletConfig) jUnmarshaller.unmarshal(is);
+        } catch (JAXBException e) {
+            log.fatal("Can't load servlet configuration!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Takes the configuration data from {@code actionServletConfig} and
+     * uses it for setting up {@code ActionFactory} and {@code FormFactory} by
+     * adding to them {@code Action} objects and {@code ActionForm} class names.
+     * @param actionServletConfig - object which contains servlet configuration data
+     * @param actionFactory - the {@code ActionFactory} instance
+     * @param formFactory - the {@code FormFactory} instance
+     */
+    void setUpFactories(ActionServletConfig actionServletConfig, ActionFactory actionFactory, FormFactory formFactory){
+        //Setting up an ActionFactory
+        for (ActionConfig actionConfig : actionServletConfig.getActions()) {
+
+            boolean needValidate = Boolean.parseBoolean(actionConfig.getValidate());
+
+            actionFactory.addAction(actionConfig.getPath(),
+                    actionConfig.getType(),
+                    needValidate,
+                    actionConfig.getInput());
+
+            if (needValidate) {
+                //Adding form info needed to this action to {@code FormFactory}
+                addFormToFormFactory(actionConfig, actionServletConfig, formFactory);
+            }
+        }
+    }
+
+    /**
+     * Takes the configuration data from {@code actionConfig} and
+     * uses it for adding {@code ActionForm} class info to {@code FormFactory}
+     * {@code IllegalArgumentException} may be thrown if configuration objects
+     * contains errors
+     * @param actionConfig - the config object which contains information
+     *                     needed for adding {@code ActionForm} to {@code FormFactory}
+     * {@code IllegalArgumentException} can be thrown if configuration contains errors
+     * @param actionServletConfig - object which contains servlet configuration data
+     * @param formFactory - the {@code FormFactory} instance
+     */
+    void addFormToFormFactory(ActionConfig actionConfig, ActionServletConfig actionServletConfig, FormFactory formFactory){
+        Optional<FormConfig> formConfigOptional = actionServletConfig.getForms()
+                .stream()
+                .filter(f -> actionConfig.getName().equals(f.getName()))
+                .findFirst();
+
+        if (formConfigOptional.isPresent()) {
+            formFactory.addFormClass(actionConfig.getPath(), formConfigOptional.get().getType());
+        } else {
+            String errorText = "In servlet config file action with validation doesn't have matching form";
+            log.fatal(errorText);
+            throw new IllegalArgumentException(errorText);
+        }
+    }
+}
---
+++ src/test/java/com/library/web/controller/ActionServletConfiguratorTest.java
@@ -0,0 +1,104 @@
+package com.library.web.controller;
+
+import com.library.web.controller.forms.ActionForm;
+import com.library.web.controller.forms.UserRegistrationForm;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ActionServletConfiguratorTest {
+
+    @Mock
+    ActionServletConfig actionServletConfig;
+
+    @Mock
+    ActionConfig actionConfig;
+
+    @Mock
+    ActionFactory actionFactory;
+
+    @Mock
+    FormFactory formFactory;
+
+    @Before
+    public void initMock(){
+
+    }
+
+    @Test
+    public void addFormToFormFactoryShouldAddForm(){
+        FormConfig testForm = new FormConfig();
+        testForm.setName("testName");
+        testForm.setType("testClassName");
+        List<FormConfig> formConfigs = Collections.singletonList(testForm);
+
+        when(actionServletConfig.getForms()).thenReturn(formConfigs);
+        when(actionConfig.getName()).thenReturn("testName");
+        when(actionConfig.getPath()).thenReturn("testPath");
+
+        new ActionServletConfigurator().addFormToFormFactory(actionConfig, actionServletConfig, formFactory);
+
+        verify(formFactory, times(1)).addFormClass("testPath", "testClassName");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void addFormToFormFactoryShouldThrowRuntimeException() {
+        FormConfig testForm = new FormConfig();
+        testForm.setName("testName");
+        List<FormConfig> formConfigs = Collections.singletonList(testForm);
+
+        when(actionServletConfig.getForms()).thenReturn(formConfigs);
+        when(actionConfig.getName()).thenReturn("anotherTestName");
+
+        new ActionServletConfigurator().addFormToFormFactory(actionConfig, actionServletConfig, formFactory);
+    }
+
+    @Test
+    public void setUpFactoriesShouldAddActionAndForm() {
+        ActionServletConfigurator configurator = spy(new ActionServletConfigurator());
+        ActionConfig action = new ActionConfig();
+        action.setInput("testInput");
+        action.setPath("testPath");
+        action.setValidate("true");
+        action.setType("testType");
+        List<ActionConfig> configs = Collections.singletonList(action);
+
+        when(actionServletConfig.getActions()).thenReturn(configs);
+        doNothing().when(configurator).addFormToFormFactory(any(), any(), any());
+
+        configurator.setUpFactories(actionServletConfig, actionFactory, formFactory);
+
+        verify(actionFactory, times(1))
+                .addAction("testPath", "testType", true, "testInput");
+
+        verify(configurator, times(1)).addFormToFormFactory(action, actionServletConfig, formFactory);
+    }
+
+    @Test
+    public void setUpFactoriesShouldAddActionAndNotAddForm() {
+        ActionServletConfigurator configurator = spy(new ActionServletConfigurator());
+        ActionConfig action = new ActionConfig();
+        action.setPath("testPath");
+        action.setValidate("false");
+        action.setType("testType");
+        List<ActionConfig> configs = Collections.singletonList(action);
+
+        when(actionServletConfig.getActions()).thenReturn(configs);
+        doNothing().when(configurator).addFormToFormFactory(any(), any(), any());
+
+        configurator.setUpFactories(actionServletConfig, actionFactory, formFactory);
+
+        verify(actionFactory, times(1))
+                .addAction("testPath", "testType", false, null);
+
+        verify(configurator, never()).addFormToFormFactory(action, actionServletConfig, formFactory);
+    }
+}
